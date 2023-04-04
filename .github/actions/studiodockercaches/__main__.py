6a704d1cfee972b10590c84923cc9e41b6180a81
Esteban Martinena Guerrero <orensbruli@gmail.com>
2023-04-04 19:35:56 +0200

    json dump

--- .github/actions/studiodockercaches/__main__.py
+++ .github/actions/studiodockercaches/__main__.py
@@ -1,4 +1,15 @@
 import sys
+import json
 
 if __name__ == "__main__":
-    print (sys.argv)
+    parent = argv[1]
+    current = arg[2]
+    image = argv[3]
+    regs=( 'DOCKERUSER', "ghcr.io/GITHUBORG" )
+    caches=( "main", parent, current )
+    result = []
+    for registry in regs:
+        for cache in caches:
+            result.append(f"type=registry,ref={registry}/{image}:{cache}-buildcache")
+    json.dumps(result)
+
