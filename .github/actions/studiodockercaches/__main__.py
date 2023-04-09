fab01d19eb1583372708a00598208461ad42ab75
Esteban Martinena Guerrero <orensbruli@gmail.com>
2023-04-09 18:40:15 +0200

    testing

--- .github/actions/studiodockercaches/__main__.py
+++ .github/actions/studiodockercaches/__main__.py
@@ -1,15 +1,19 @@
-import sys
-import json
+import argparse
 
 if __name__ == "__main__":
-    parent = sys.argv[1]
-    current = sys.argv[2]
-    image = sys.argv[3]
-    regs=sys.argv[4].split(',')
-    caches=( "main", parent, current )
-    result = []
-    for registry in regs:
-        for cache in caches:
-            print(f"type=registry,ref={registry}/{image}:{cache}-buildcache")
-    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("parent")
+    parser.add_argument("current")
+    parser.add_argument("image")
+    parser.add_argument("regs", nargs="+")
+    args = parser.parse_args()
 
+    caches = ("main", arg.parent, arg.current)
+    result = {'cache-from':[], "cache-to":[]}
+    for registry in args.regs:
+        for cache in caches:
+            result["cache-from"].append(f"type=registry,ref={registry}/{args.image}:{cache}-buildcache")
+            if cache == arg.current:
+                result["cache-to"].append(f"type=registry,ref={registry}/{args.image}:{cache}-buildcache")
+    result["cache-from"] = "\n".join(result["cache-from"])
+    result["cache-to"] = "\n".join(result["cache-to"])
