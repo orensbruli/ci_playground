9c09fb302d72fe8147a70514e2706b0aca3fa826
Esteban Martinena Guerrero <orensbruli@gmail.com>
2023-04-09 18:44:53 +0200

    fix args

--- .github/actions/studiodockercaches/__main__.py
+++ .github/actions/studiodockercaches/__main__.py
@@ -9,12 +9,12 @@ if __name__ == "__main__":
     parser.add_argument("regs", nargs="+")
     args = parser.parse_args()
 
-    caches = ("main", arg.parent, arg.current)
+    caches = ("main", args.parent, args.current)
     result = {'cache-from':[], "cache-to":[]}
     for registry in args.regs:
         for cache in caches:
             result["cache-from"].append(f"type=registry,ref={registry}/{args.image}:{cache}-buildcache")
-            if cache == arg.current:
+            if cache == args.current:
                 result["cache-to"].append(f"type=registry,ref={registry}/{args.image}:{cache}-buildcache")
     result["cache-from"] = "\n".join(result["cache-from"])
     result["cache-to"] = "\n".join(result["cache-to"])
